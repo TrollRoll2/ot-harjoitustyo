42b4d5036dbaf901b34a8ed04fde32f88a9dcf1e
Patrik Backman <patrik.backman@helsinki.fi>
2022-03-30 00:06:05 +0300

    Revert "revert of revert hopefully"
    This reverts commit 3a212bf3fa0d4c0680b2415a25548876ca167eda.

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,13 +7,9 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
 <<<<<<< HEAD
-<<<<<<< HEAD
         self.assertNotEqual(self.maksukortti, None)
 =======
         self.assertNotEqual(self.maksukortti, None)
 
     
 >>>>>>> tmp
-=======
-        self.assertNotEqual(self.maksukortti, None)
->>>>>>> parent of 0d2dd4e... Tehtävät -> 7
